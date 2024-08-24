208010a2c7210c5c9a597c3d18e02b77b5c0bbd8
An525ish <an525ish@gmail.com>
2024-08-25 02:15:35 +0530

    fix ui bug in 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,9 +5,9 @@ const NotFound = () => {
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
             <h1 className="text-6xl font-bold text-gray-800">404</h1>
             <p className="text-2xl text-gray-700 mt-4">Page not found</p>
-            <p className="mt-2 text-lg text-gray-600">Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>
-            <Link to="/" className="mt-6 text-indigo-500 hover:text-indigo-600 text-lg font-semibold">
-                Go back home
+            <p className="mt-2 text-lg text-center text-grey/60">Sorry, we couldn&apos;t find the page you&apos;re looking for.</p>
+            <Link to="/" className="mt-6 text-blue hover:text-blue/80 text-lg font-semibold">
+                <span className='mr-1'>〈</span> Go back home
             </Link>
         </div>
     );
